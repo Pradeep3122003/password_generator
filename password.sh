13016e304c11a796563e2976d8e0228218a00d89
Pradeep3122003 <pradeep@PRADEEP.localdomain>
2025-07-02 20:04:59 +0000

    password script

---
+++ password.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+charset=""
+length=8
+
+for arg in "$@"; do
+  case $arg in
+    -n) charset+="0-9" ;;
+    -a) charset+="a-z" ;;
+    -A) charset+="A-Z" ;;
+    -S) charset+="!@#$%^&*()_+=-{}[]:;<>,.?/" ;;
+    [0-9]*) length=$arg ;;
+  esac
+done
+
+if [ -z "$charset" ]; then
+  charset="a-zA-Z0-9!@#$%^&*()_+=-{}[]:;<>,.?/"
+fi
+
+if [ "$length" -lt 5 ]; then
+  < /dev/urandom tr -dc "$charset" | head -c "$length"
+  echo
+  exit 0
+fi
+
+build_password() {
+  result=""
+  [[ "$charset" == *"a-z"* ]] && result+=$(< /dev/urandom tr -dc 'a-z' | head -c1)
+  [[ "$charset" == *"A-Z"* ]] && result+=$(< /dev/urandom tr -dc 'A-Z' | head -c1)
+  [[ "$charset" == *"0-9"* ]] && result+=$(< /dev/urandom tr -dc '0-9' | head -c1)
+  [[ "$charset" == *"!"* ]]   && result+=$(< /dev/urandom tr -dc '!@#$%^&*()_+=-{}[]:;<>,.?/' | head -c1)
+  restlen=$((length - ${#result}))
+  [[ $restlen -gt 0 ]] && result+=$(< /dev/urandom tr -dc "$charset" | head -c "$restlen")
+
+  echo "$result" | fold -w1 | shuf | tr -d '\n'
+  echo
+}
+
+build_password
