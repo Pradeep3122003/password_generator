83d856489cca6cad2b5b5cf9c3e723192062486d
Pradeepkh312 <pradeepkh312@gmail.com>
2025-11-14 18:36:03 +0530

    updated python code to generate strong password using library

--- password.py
+++ password.py
@@ -1,39 +1,48 @@
-import random, sys, pathlib
-from collections import defaultdict
+import random, sys, pathlib, gzip
 
-def system_wordlist(path="/usr/share/dict/words"):
+def gz_wordlist(path):
     p = pathlib.Path(path)
     if not p.exists():
         return []
-    return [w.strip() for w in p.read_text(encoding="utf-8", errors="ignore").splitlines() if w.strip().isalpha()]
+
+    with gzip.open(p, "rt", encoding="utf-8", errors="ignore") as f:
+        return [w.strip() for w in f.readlines() if w.strip().isalpha()]
 
 def word(wordlist):
     if not wordlist:
-        return None
+        return ""
     return random.choice(wordlist)
 
+
 def number(seed):
- return random.randint(2,seed)
+    return random.randint(2, seed)
 
-def generate():
-  sys_words = system_wordlist()
-  password=""
-  length=random.randint(3,8)
-  for i in range(length):
-    toss=random.randint(0,3)
-    if toss==0 or toss==1:
-      password+=word(sys_words)
-    else:
-      password+=str(number(3333))
+def generate(wordlist):
+    password = ""
+    length = random.randint(3, 8)
 
-  return password
+    for _ in range(length):
+        toss = random.randint(0, 3)
+
+        if toss == 0 or toss == 1:
+            password += word(wordlist)
+        else:
+            password += str(number(3333))
+
+    return password
+
 
 if __name__ == "__main__":
+    words = gz_wordlist("./source/words.txt.gz")
+
     if len(sys.argv) > 1:
-      length = int(sys.argv[1])
+        maxlen = int(sys.argv[1])
     else:
-      length = 0
-    password=generate()
-    if length>1:
-     password=password[:length]
-    print("password: "+password)
+        maxlen = 0
+
+    password = generate(words)
+
+    if maxlen > 1:
+        password = password[:maxlen]
+
+    print("password:", password)
